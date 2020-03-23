525e398034afbac076f4c4cc14ff964df42ecc31
Faye Carter <fayecarter333@gmail.com>
2020-03-23 15:25:22 +0000

    Oystercard initializes with default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,8 @@
-describe Oystercard do
+require './lib/oystercard'
 
+describe Oystercard do
+  it 'oystercard has a balance of 0' do
+    expect(Oystercard.new).to have_attributes(:balance => 0)
+  end
 end
+
